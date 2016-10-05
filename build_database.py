fe578ede7e604a4b932a5b1a04dc7bf0830cc824
Conrad Storz <conradstorz@gmail.com>
2016-10-05 09:04:45 -0400

    downloading files into structured directory complete

--- build_database.py
+++ build_database.py
@@ -23,7 +23,7 @@ def Recover_file(filename):
 def extract_title(string):
     """ Assumes that the given string is formatted like a URL.
     The end of the URL is assumed to have a "." extension.
-    What remains after the extension is then normalized with
+    What remains before the extension is then normalized with
     space characters as word seperators.
     """
     splt_str = string.split('/')
@@ -63,7 +63,7 @@ def build_dictionary_from(filename):
 def extract_embeded_datestr(string):
     """ Takes an input string and scans it for yalid date strings.
     The first valid string found is returned as a datetime object.
-    If no date is found then a prdictable default date is returned.
+    If no date is found then a predictable default date is returned.
     """
     DEFAULT = parse('1936-07-25', yearfirst=True)
     DADS_BDAY = parse('1936-07-25 12:34:56', yearfirst=True)
@@ -79,31 +79,86 @@ def extract_embeded_datestr(string):
         date = DADS_BDAY
     return date
 
-   
-def Run():
-    file = '/home/conrad/Programming_Code/Python/oldradioworld/2016-09-15 21:43:50.333220.json'
+
+def put_show_in_database(db, episode):
+    """ Show represented by episode is parsed for date and then date is used
+    as key for database of lists of show with same date.
+    """
+    air_date = extract_embeded_datestr(episode)
+
+    if air_date.month in db:
+        if air_date.day in db[air_date.month]:
+            if air_date.year in db[air_date.month][air_date.day]:
+                db[air_date.month][air_date.day][air_date.year] = {episode: False}
+            else:
+                db[air_date.month][air_date.day] = {air_date.year: {episode: False}}
+        else:
+            db[air_date.month] = {air_date.day: {air_date.year: {episode: False}}}
+    else:
+        db[air_date.month] = {air_date.day: {air_date.year: {episode: False}}}
+
+    return air_date
+
+
+def Build_an_mp3_database(file):
+    date_database = {}
     shows = 0
     episodes = 0
-    earliest_date = parse('2016-1-1')
-    latest_date = parse('1900-1-1')
     dictionary_of_shows = build_dictionary_from(file)
     sd = dictionary_of_shows 
+
     for k in sd:
         shows += 1
         show_name = extract_title(k)
         print('{:4d} MP3s in {} "{}"'.format(len(sd[k]), show_name, k))
         for episode in sd[k]:
             episodes += 1
-            air_date = extract_embeded_datestr(episode)
-            if air_date > latest_date:
-                latest_date = copy.copy(air_date)
-            if air_date < earliest_date:
-                earliest_date = copy.copy(air_date)
-            print('{}: {}'.format(show_name, air_date))
+            date_database[episode] = False
+
     print('{} total shows'.format(shows))
     print('{} total episodes'.format(episodes))
-    print('Earliest air date: {}'.format(earliest_date))
-    print('Last air date: {}'.format(latest_date))
+    print('{} items in database.'.format(len(date_database)))
+    finished_time = time.now()
+    print(finished_time)
+    sleep(1)
+    filename = '{}.json'.format(finished_time)
+    with open(filename, 'w') as fp:
+        json.dump(date_database, fp)
+    print('Results saved as {}'.format(filename))
+
+
+def build_a_detailed_database(file):
+    date_database = {}
+    shows = 0
+    episodes = 0
+    dictionary_of_shows = build_dictionary_from(file)
+    sd = dictionary_of_shows 
+
+    for k in sd:
+        shows += 1
+        show_name = extract_title(k)
+        print('{:4d} MP3s in {} "{}"'.format(len(sd[k]), show_name, k))
+        for episode in sd[k]:
+            episodes += 1
+            aired = put_show_in_database(date_database, episode)
+
+    print('{} total shows'.format(shows))
+    print('{} total episodes'.format(episodes))
+    print('{} items in database.'.format(len(date_database)))
+    finished_time = time.now()
+    print(finished_time)
+    sleep(1)
+    filename = '{}.json'.format(finished_time)
+    with open(filename, 'w') as fp:
+        json.dump(date_database, fp)
+    print('Results saved as {}'.format(filename))
+
+
 
 if __name__ == '__main__':
-    Run()
+    file = '/home/conrad/Programming_Code/Python/oldradioworld/2016-09-15 21:43:50.333220.json'
+
+    #Build_an_mp3_database(file)
+
+    build_a_detailed_database(file)
+
